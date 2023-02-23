f68b5226e355c9b711bc06f96175b6f00ccd4b61
ravelnogueira <ravel.santos@hotmail.com.br~>
2023-02-23 08:09:55 -0300

    Finish

---
+++ ValidaCpf/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o CPF a ser validado: ");
+        string cpf = Console.ReadLine();
+
+        if (ValidaCPF(cpf))
+        {
+            Console.WriteLine("CPF válido!");
+        }
+        else
+        {
+            Console.WriteLine("CPF inválido!");
+        }
+    }
+
+    static bool ValidaCPF(string cpf)
+    {
+        CPFValidator cpfValidator = new CPFValidator();
+        return cpfValidator.ValidaCPF(cpf);
+    }
+}
---
+++ ValidaCpf/ValidaCPF.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class CPFValidator
+{
+    public bool ValidaCPF(string cpf)
+    {
+        cpf = cpf.Replace(".", "").Replace("-", ""); // remove caracteres especiais
+
+        if (cpf.Length != 11) // verifica se o CPF tem 11 dígitos
+        {
+            return false;
+        }
+
+        // calcula o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++)
+        {
+            soma += int.Parse(cpf[i].ToString()) * (10 - i);
+        }
+        int resto = soma % 11;
+        int digito1 = resto < 2 ? 0 : 11 - resto;
+
+        if (int.Parse(cpf[9].ToString()) != digito1) // verifica se o primeiro dígito está correto
+        {
+            return false;
+        }
+
+        // calcula o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            soma += int.Parse(cpf[i].ToString()) * (11 - i);
+        }
+        resto = soma % 11;
+        int digito2 = resto < 2 ? 0 : 11 - resto;
+
+        if (int.Parse(cpf[10].ToString()) != digito2) // verifica se o segundo dígito está correto
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
